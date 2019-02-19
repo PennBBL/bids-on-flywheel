55eaa6a5d71c4b45a7681d36f8fd12b1d788561f
Tinashe Michael Tapera <teetaps@yahoo.com>
2019-02-19 16:33:21 -0500

    notebook of query complete

--- scripts/query_bids_validity.py
+++ scripts/query_bids_validity.py
@@ -13,7 +13,7 @@ def query_fw(project, client):
         return None
 
 
-def ExtractBidsValidity(acquisitionID, client):
+def ExtractBidsValidity(acquisitionID, client, verbose=True):
     '''
     A helper function to dig into the file.info object
     (a dictionary of dictionaries) and extract the BIDS validity value
@@ -22,12 +22,12 @@ def ExtractBidsValidity(acquisitionID, client):
     try:
         acq = client.get(acquisitionID)
     except:
-        print("There may not be any data for this acquisition!")
+        if verbose: print("There may not be any data for this acquisition!")
         return None
     niftis = [x for x in acq.files if x['type'] == 'nifti']
     # if there are no niftis, return
     if (len(niftis) < 1):
-        print("No BIDs classification for this acquisition yet")
+        if verbose: print("No BIDs classification for this acquisition yet")
         return None
     else:
         df = []
@@ -45,25 +45,25 @@ def ExtractBidsValidity(acquisitionID, client):
         return(df)
 
 
-def query_bids_validity(project, client):
+def query_bids_validity(project, client, verbose=True):
 
     # first, log in to flywheel
     print("Connecting to flywheel server...")
-    assert fw
+    assert client
     # query flywheel for the argument
     print("Querying server...")
-    result = query_fw(project, fw)
+    result = query_fw(project, client)
 
     # get the subjects for this project
-    view = fw.View(columns='subject')
-    subject_df = fw.read_view_dataframe(view, result.id)
+    view = client.View(columns='subject')
+    subject_df = client.read_view_dataframe(view, result.id)
 
     # loop through the subjects to extract acquisitions
     print("Processing acquisitions...")
     sessions = []
-    view = fw.View(columns='acquisition')
+    view = client.View(columns='acquisition')
     for ind, row in subject_df.iterrows():
-        session = fw.read_view_dataframe(view, row["subject.id"])
+        session = client.read_view_dataframe(view, row["subject.id"])
         if(session.shape[0] > 0):
             sessions.append(session)
 
@@ -73,7 +73,7 @@ def query_bids_validity(project, client):
     print("Extracting BIDS information...")
     bids_classifications = []
     for ind, row in acquisitions.iterrows():
-        temp_info = ExtractBidsValidity(row["acquisition.id"], fw)
+        temp_info = ExtractBidsValidity(row["acquisition.id"], client, verbose)
         if temp_info is not None:
             bids_classifications.extend(temp_info)
     bids_classifications = pd.DataFrame(bids_classifications)
